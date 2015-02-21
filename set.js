1e1c8d076d087fb4cf1a2a001f7652ad1c037a43
Andrew Borovin <a@borovin.com>
2015-02-21 21:25:59 +0300

    set only changed props

--- set.js
+++ set.js
@@ -1,15 +1,43 @@
-define(function(require) {
+define(function (require) {
     //requirements
     var deepExtend = require('bower_components/deepExtend/deepExtend'),
         _ = require('bower_components/lodash/lodash');
 
-    function pathToObject(path, value){
+    function getChanges(newData, oldData) {
+
+        var changes = {};
+
+        if (newData === oldData) {
+            return {};
+        }
+
+        if (!_.isPlainObject(newData) || !_.isPlainObject(oldData)) {
+            return newData;
+        }
+
+        _.forOwn(newData, function (value, key) {
+            if (_.isPlainObject(value)) {
+                changes[key] = getChanges(value, oldData[key]);
+
+                if (_.isEmpty(changes[key]) && !_.isEmpty(value)) {
+                    delete changes[key];
+                }
+
+            } else if (oldData[key] !== value) {
+                changes[key] = value;
+            }
+        });
+
+        return changes;
+    }
+
+    function pathToObject(path, value) {
         var object = {},
             attr = object,
             segments = path.split('.');
 
-        _.each(segments, function(segment, index){
-            if (index === segments.length - 1){
+        _.each(segments, function (segment, index) {
+            if (index === segments.length - 1) {
                 attr[segments[segments.length - 1]] = value;
             } else {
                 attr[segment] = {};
@@ -23,7 +51,7 @@ define(function(require) {
     function set(object, path, newData, extra) {
 
         if (_.isPlainObject(newData)) {
-            _.forEach(newData, function(value, pathPart) {
+            _.forEach(newData, function (value, pathPart) {
                 set(object, (path ? path + '.' : '') + pathPart, value, extra);
             });
         } else {
@@ -31,17 +59,21 @@ define(function(require) {
         }
     }
 
-    return function(object, path, data, extra) {
+    return function (object, path, data, extra) {
 
-        if (typeof path === 'string'){
+        var changedData;
+
+        if (typeof path === 'string') {
             data = pathToObject(path, data);
         } else {
             extra = data;
             data = path;
         }
 
-        set(object, null, data, extra);
+        changedData = getChanges(data, object);
 
-        return data;
+        set(object, null, changedData, extra);
+
+        return changedData;
     }
 });
--- tests/spec.js
+++ tests/spec.js
@@ -13,5 +13,24 @@ define(function(require, exports, module) {
             expect(obj.a.b.c).toBe('abc');
         });
 
+        it('Установка значиния в частично заполненный объект', function(){
+
+            var obj = {
+                a: {
+                    b: 'b',
+                    c: 'c',
+                    d: 'd'
+                }
+            };
+
+            set(obj, 'a', {
+                e: 'e',
+                b: 'f'
+            });
+
+            expect(obj.a.e).toBe('e');
+            expect(obj.a.b).toBe('f');
+        });
+
     });
 });
