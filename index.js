eff60cc9c97b125db324b5b3f3d65539cd6eaf87
Andrew Borovin <a@borovin.com>
2016-11-03 00:14:18 +0300

    add full lodash package to deps

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const get = require('@basket/get');
-const _set = require('lodash.set');
-const _mergeWith = require('lodash.mergewith');
-const _cloneDeep = require('lodash.clonedeep');
-const _isPlainObject = require('lodash.isplainobject');
+const _set = require('lodash/set');
+const mergeWith = require('lodash/mergewith');
+const cloneDeep = require('lodash/clonedeep');
+const isPlainObject = require('lodash/isplainobject');
 const isArray = Array.isArray;
 
 function merge(...args) {
-    return _mergeWith(...args, (objValue, srcValue) => {
+    return mergeWith(...args, (objValue, srcValue) => {
         if (isArray(srcValue)) {
-            return _cloneDeep(srcValue);
+            return cloneDeep(srcValue);
         }
     });
 }
 
 function set(object, path, value) {
-    if (_isPlainObject(path)) {
+    if (isPlainObject(path)) {
         return merge(object, path);
     }
 
@@ -25,11 +25,11 @@ function set(object, path, value) {
     let oldValue = get(object, path);
     let newValue = value;
 
-    if (_isPlainObject(oldValue) && _isPlainObject(newValue)) {
+    if (isPlainObject(oldValue) && isPlainObject(newValue)) {
         newValue = merge({}, oldValue, newValue);
     }
 
-    return _set(object, path, _cloneDeep(newValue));
+    return _set(object, path, cloneDeep(newValue));
 }
 
 module.exports = set;
